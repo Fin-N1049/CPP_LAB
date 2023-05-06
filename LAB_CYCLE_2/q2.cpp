d4b850497c54594a64a51a64df98161f26f17fd0
jeslinpjames <jeslinpjames007@gmail.com>
2023-05-06 16:07:32 +0530

    Cycle 2 and 3 code beautified

--- LAB_CYCLE_2/q2.cpp
+++ LAB_CYCLE_2/q2.cpp
@@ -1,85 +1,72 @@
 #include<iostream>
+
 using namespace std;
-class complex
-{
-    int real,imag;
-    public:
-    void input()
-    {
-        cout<<"enter the value of real part of the complex :";
-        cin>>real;
-        cout<<"enter the value of imaginary part ofthe complex :";
-        cin>>imag;
+class complex {
+  int real, imag;
+  public:
+    void input() {
+      cout << "enter the value of real part of the complex :";
+      cin >> real;
+      cout << "enter the value of imaginary part ofthe complex :";
+      cin >> imag;
     }
-    void display()
-    {
-       if(imag<0)
-       cout<<real<<imag<<"i"<<endl<<endl;
-       else
-       cout<<real<<"+"<<imag<<"i"<<endl<<endl;
-       
-    }
-    friend complex operator+(complex a,complex b);
-    friend complex operator*(complex a,complex b);
-    
+  void display() {
+    if (imag < 0)
+      cout << real << imag << "i" << endl << endl;
+    else
+      cout << real << "+" << imag << "i" << endl << endl;
+
+  }
+  friend complex operator + (complex a, complex b);
+  friend complex operator * (complex a, complex b);
+
 };
- complex operator+(complex a,complex b)
-    {
-        complex c;
-        c.real=a.real+b.real;
-        c.imag=a.imag+b.imag;
-        return c;
-    }
-      complex operator*(complex a,complex b)
-    {
-        complex c;
-        c.real=(a.real*b.real)-(a.imag*b.imag);
-        c.imag=(a.real*b.imag)+(b.real*a.imag);
-        return c;
-    }
-int main()
-{
-    char option;
-    int choice;
-    do{
-    complex c1,c2,c3;
-    cout<<"Complex 1"<<endl;
+complex operator + (complex a, complex b) {
+  complex c;
+  c.real = a.real + b.real;
+  c.imag = a.imag + b.imag;
+  return c;
+}
+complex operator * (complex a, complex b) {
+  complex c;
+  c.real = (a.real * b.real) - (a.imag * b.imag);
+  c.imag = (a.real * b.imag) + (b.real * a.imag);
+  return c;
+}
+int main() {
+  char option;
+  int choice;
+  do {
+    complex c1, c2, c3;
+    cout << "Complex 1" << endl;
     c1.input();
-    cout<<"Complex 2"<<endl;
+    cout << "Complex 2" << endl;
     c2.input();
-    cout<<"Complex 1 :";
+    cout << "Complex 1 :";
     c1.display();
-    cout<<endl<<"Complex 2 :";
+    cout << endl << "Complex 2 :";
     c2.display();
-    cout<<"enter 1 to perform addition of the two complexes  :\n";
-    cout<<"enter 2 to perform multiplication of the two complexes  :\n";
-    cin>>choice;
-    if(choice==1)
-    {
-        c3=c1+c2;
-        cout<<endl<<"sum of the two complex  :";
-        c3.display();
+    cout << "enter 1 to perform addition of the two complexes  :\n";
+    cout << "enter 2 to perform multiplication of the two complexes  :\n";
+    cin >> choice;
+    if (choice == 1) {
+      c3 = c1 + c2;
+      cout << endl << "sum of the two complex  :";
+      c3.display();
+    } else if (choice == 2) {
+      c3 = c1 * c2;
+      cout << endl << "product of the two complex :";
+      c3.display();
+    } else {
+      cout << "invalid option,re-enter option";
     }
-    else if(choice==2)
-    {
-        c3=c1*c2;
-        cout<<endl<<"product of the two complex :";
-        c3.display();
+    cout << "do you want to continue the program ?(y/n)  :";
+    cin >> option;
+    if (option == 'n') {
+      cout << "-----thank you-----";
+    } else {
+
     }
-    else
-    {
-        cout<<"invalid option,re-enter option";
-    }
-    cout<<"do you want to continue the program ?(y/n)  :";
-    cin>>option;
-    if (option=='n')
-    {
-        cout<<"-----thank you-----";
-    }
-    else
-    {
-        
-    }
-    }while(option=='y');
-    return 0 ;
+  } while (option == 'y');
+  return 0;
 }
--- LAB_CYCLE_2/q6.cpp
+++ LAB_CYCLE_2/q6.cpp
@@ -1,136 +1,110 @@
 #include<iostream>
+
 #include<cstring>
+
 using namespace std;
-class Student
-{
-    protected:
-    int roll_no;
-    char name[15];
-    public:
-    Student()
-    {
-    }
-    Student(int r,char n[])
-    {
-        roll_no=r;
-        strcpy(name,n);
-        
-    }
-     void show_details()
-    {
-        cout<<"name of the student :"<<name;
-        cout<<"roll.no. of the student :"<<roll_no;
-    }
-    
-};
-class test:virtual public Student
-{
-    protected:
-    int mark1,mark2,mark3;
-    public:
-    test(int m,int o,int p)
-    {
-        mark1=m;
-        mark2=o;
-        mark3=p;
-    }
-    void show_marks()
-    {
-        cout<<"mark of student in maths="<<mark1<<endl;
-        cout<<"mark of student in english="<<mark2<<endl;
-        cout<<"mark of student in physics="<<mark3<<endl;
-    }
-};
-class sports:virtual public Student
-{
-    protected:
-    int score;
-    public:
-    sports(int s)
-    {
-        score=s;
-    }
+class Student {
+  protected: int roll_no;
+  char name[15];
+  public: Student() {}
+  Student(int r, char n[]) {
+    roll_no = r;
+    strcpy(name, n);
 
-    void show_score()
-    {
-        cout<<"score of student in sports="<<score<<endl;
-    }
-};
-class result:public test,public sports
-{
-    int total;
-    public:
+  }
+  void show_details() {
+    cout << "name of the student :" << name;
+    cout << "roll.no. of the student :" << roll_no;
+  }
 
-    result(int r,char n[],int m,int o,int p,int s):test(m,o,p),sports(s),Student(r,n)
-    {
-        
-    }
-    void calc_result ()
-    {
-      total=mark1+mark2+mark3+score;  
-    }
-    void display()
-    {
-       show_details();
-       show_marks();
-       show_score();
-       calc_result();
-       cout<<"overall score of student="<<total<<endl<<endl;
-    }
 };
-int main()
-{
-    int n;
-    cout<<"enter number of students  :";
-    cin>>n;
-    for (int i=0;i<n;i++)
-    {
-        cout<<"-----STUDENT  "<<i+1<<"------\n\n";
-    int r,m,o,p,s;
+class test: virtual public Student {
+  protected: int mark1,
+  mark2,
+  mark3;
+  public: test(int m, int o, int p) {
+    mark1 = m;
+    mark2 = o;
+    mark3 = p;
+  }
+  void show_marks() {
+    cout << "mark of student in maths=" << mark1 << endl;
+    cout << "mark of student in english=" << mark2 << endl;
+    cout << "mark of student in physics=" << mark3 << endl;
+  }
+};
+class sports: virtual public Student {
+  protected: int score;
+  public: sports(int s) {
+    score = s;
+  }
+
+  void show_score() {
+    cout << "score of student in sports=" << score << endl;
+  }
+};
+class result: public test, public sports {
+  int total;
+  public:
+
+    result(int r, char n[], int m, int o, int p, int s): test(m, o, p), sports(s), Student(r, n) {
+
+    }
+  void calc_result() {
+    total = mark1 + mark2 + mark3 + score;
+  }
+  void display() {
+    show_details();
+    show_marks();
+    show_score();
+    calc_result();
+    cout << "overall score of student=" << total << endl << endl;
+  }
+};
+int main() {
+  int n;
+  cout << "enter number of students  :";
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cout << "-----STUDENT  " << i + 1 << "------\n\n";
+    int r, m, o, p, s;
     char n[15];
-    cout<<"enter the name of the student :";
-    cin>>n;
-    cout<<"\nenter the roll.no of the student :";
-    cin>>r;
-    cout<<"\nenter the mark of student in maths :";
-    cin>>m;
-    if(m>50)
-    {cout<<"error!!!please enter correct scores";
-    cin>>m;}
-    else
-    {
+    cout << "enter the name of the student :";
+    cin >> n;
+    cout << "\nenter the roll.no of the student :";
+    cin >> r;
+    cout << "\nenter the mark of student in maths :";
+    cin >> m;
+    if (m > 50) {
+      cout << "error!!!please enter correct scores";
+      cin >> m;
+    } else {}
+    cout << "enter the mark of student in english :";
+    cin >> o;
+    if (o > 50) {
+      cout << "error!!!please enter correct scores" << endl;
+      cin >> o;
+    } else {} {
+      cout << "enter the mark of student in physics :";
+      cin >> p;
     }
-     cout<<"enter the mark of student in english :";
-    cin>>o;
-    if(o>50)
-    {cout<<"error!!!please enter correct scores"<<endl;
-    cin>>o;}
-    else
-    {
-    }
-     {cout<<"enter the mark of student in physics :";
-    cin>>p;}
-    if(p>50)
-    {cout<<"error!!!please enter correct scores"<<endl;
-    cin>>p;}
-    else
-    {
-    }
-     cout<<"enter the score of student in sports :";
-    cin>>s;
-    if((s>50))
-    {cout<<"error!!!please enter correct scores"<<endl;
-    cin>>s;}
-    else
-    {
-    }
-    result rt(r,n,m,o,p,s);
-    cout<<"\ndetails of student "<<i+1<<endl;
-    cout<<"------------------------------------"<<endl;
+    if (p > 50) {
+      cout << "error!!!please enter correct scores" << endl;
+      cin >> p;
+    } else {}
+    cout << "enter the score of student in sports :";
+    cin >> s;
+    if ((s > 50)) {
+      cout << "error!!!please enter correct scores" << endl;
+      cin >> s;
+    } else {}
+    result rt(r, n, m, o, p, s);
+    cout << "\ndetails of student " << i + 1 << endl;
+    cout << "------------------------------------" << endl;
     rt.display();
-    
-    }
-    cout<<"..............THANK YOU.............."<<endl;
-    
-    return 0;
+
+  }
+  cout << "..............THANK YOU.............." << endl;
+
+  return 0;
 }
